b4551d0db048e1079f49a0cb024e25900d7b76e6
Lee Adcock <lee@katieandlee.com>
2017-04-04 17:11:11 -0400

    Adding 'FavoriteIcecream' intent handler

--- alexaLambda1.js
+++ alexaLambda1.js
@@ -3,6 +3,9 @@ const alexa = require('alexa-sdk');
 const handlers = {
     'LaunchRequest': function () {
         this.emit(':ask', "Welcome to your cookbook, ask me some questions and I'll try to answer!");
+    },
+    'FavoriteIcecream': function () {
+        this.emit(':ask', "I love all types of icecream, but rocky road is my favorite!");
     }
 };
 

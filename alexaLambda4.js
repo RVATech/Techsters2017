c59af115f65f787b0a56a214822baa608a728106
Lee Adcock <lee@katieandlee.com>
2017-04-05 06:49:50 -0600

    Create alexaLambda4.js

---
+++ alexaLambda4.js
@@ -0,0 +1,48 @@
+const alexa = require('alexa-sdk');
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit(':ask', "Welcome to your cookbook, ask me some questions and I'll try to answer!");
+    },
+    'FavoriteIcecream': function () {
+        this.emit(':ask', "I love all types of icecream, but rocky road is my favorite!");
+    },
+    'KitchenFacts': function () {
+        const facts = [
+            'Pringles once had a lawsuit trying to prove that they weren’t really potato chips.',
+            'Ripe cranberries will bounce like rubber balls.',
+            'An average ear of corn has an even number of rows, usually 16.',
+            'Apples belong to the rose family, as do pears and plums.',
+            'One of the most popular pizza toppings in Brazil is green peas.'
+        ];
+        const factNumber = Math.floor(Math.random() * facts.length);
+
+        this.emit(':tell', facts[factNumber]);
+    },
+    'RecipeFinder': function() {
+        const ingredient = this.event.request.intent.slots.Item.value;
+        var handler = this;
+        var http = require('http');
+
+        var url='http://food2fork.com/api/search?key=fa766e3612ffd0bb81207883147518f4&q='+ingredient;
+
+        http.get(url, function(res) {
+            var body = '';
+            res.on('data', function(chunk) { body += chunk; });
+            res.on('end', function() {
+                const recipies = JSON.parse(body);
+                const recipeNumber = Math.floor(Math.random() * recipies.count);
+                handler.emit(':tell', recipies.recipes[recipeNumber].title);
+                handler.context.succeed();
+            });
+        }).on('error', function(e) {
+            handler.context.done(null, 'FAILURE');
+        });
+    }
+};
+
+exports.handler = (event, context) => {
+    const handler = alexa.handler(event, context);
+    handler.registerHandlers(handlers);
+    handler.execute();
+};
